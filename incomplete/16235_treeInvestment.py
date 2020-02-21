4305517dbea5eef6027d5e82736142fb7808b8f6
iotlab <gw4621@gmail.com>
2020-02-21 17:36:14 +0900

    Time out: need optimization

--- incomplete/16235_treeInvestment.py
+++ incomplete/16235_treeInvestment.py
@@ -4,8 +4,9 @@ A = []
 tree = [[[] for _ in range(N+1)] for _ in range(N+1)]
 deadTree = []
 nutrient = [[5 for _ in range(N+1)] for _ in range(N+1)]
+A.append([])
 for _ in range(N):
-	A.append(list(map(int, sys.stdin.readline().split())))
+	A.append([0]+list(map(int, sys.stdin.readline().split())))
 for _ in range(M):
 	x, y, z = list(map(int, sys.stdin.readline().split()))
 	tree[x][y].append(z)
@@ -15,12 +16,12 @@ def simSpring():
 	for r in range(1, N+1):
 		for c in range(1, N+1):
 			for i in range(len(tree[r][c])-1, -1, -1):
-				if tree[r][c][i]+1 < nutrient[r][c]:
-					tree[r][c][i] += 1
+				if tree[r][c][i] <= nutrient[r][c]:
 					nutrient[r][c] -= tree[r][c][i]
+					tree[r][c][i] += 1
 				else:
 					for j in range(0, i+1):
-						deadTree.append((r, c, tree[r][c][j]+1))
+						deadTree.append((r, c, tree[r][c][j]))
 					del tree[r][c][:i+1]
 					break
 
@@ -29,16 +30,63 @@ def simSummer():
 		nutrient[i[0]][i[1]] += int(i[2] / 2)
 	deadTree.clear()
 
-def corner_edge():
-
-def simFall():
+def rep_corner_edge(r, c):
+	for t in tree[r][c]:
+		if t % 5 == 0:
+			for i in range(-1, 2):
+				for j in range(-1, 2):
+					if r+i >= 1 and r+i <= N and c+j >= 1 and c+j <= N and not (i==0 and j==0):
+						tree[r+i][c+j].append(1)
 	
 
-def simWinter():
+def rep_center(r, c):
+	for t in tree[r][c]:
+		if t % 5 == 0:
+			tree[r-1][c-1].append(1)
+			tree[r-1][c].append(1)
+			tree[r-1][c+1].append(1)
+			tree[r][c-1].append(1)
+			tree[r][c+1].append(1)
+			tree[r+1][c-1].append(1)
+			tree[r+1][c].append(1)
+			tree[r+1][c+1].append(1)
 
+def simFall():
+	rep_corner_edge(1, 1)
+	rep_corner_edge(1, N)
+	rep_corner_edge(N, 1)
+	rep_corner_edge(N, N)
+	for i in range(2, N):
+		rep_corner_edge(1, i)
+		rep_corner_edge(N, i)
+		rep_corner_edge(i, 1)
+		rep_corner_edge(i, N)
+		for j in range(2, N):
+			rep_center(i, j)
+
+def simWinter():
+	for i in range(1, N+1):
+		for j in range(1, N+1):
+			nutrient[i][j] += A[i][j]
 
 def simYear():
+	simSpring()
+	simSummer()
+	simFall()
+	simWinter()
 
+def countTree():
+	total = 0
+	for i in tree:
+		for j in i:
+			total += len(j)
+	return total
 
 def solve():
-	while 
+	year = 0
+	while year < K:
+		simYear()
+		year += 1
+	print(countTree())
+
+solve()
