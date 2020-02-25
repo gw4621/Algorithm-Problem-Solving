9bd8d1892b35cbe105620f89c043077c1d15c921
iotlab <gw4621@gmail.com>
2020-02-25 18:00:16 +0900

    Not correct

--- incomplete/17140_2drowcolCalc.py
+++ incomplete/17140_2drowcolCalc.py
@@ -1,13 +1,11 @@
 from collections import defaultdict
 
 r, c, k = list(map(int, input().split()))
+r -= 1
+c -= 1
 A = []
 for _ in range(3):
 	A.append(list(map(int, input().split())))
-rowNum = 3
-colNum = 3
-
-
 
 def rowCal(arr):
 	newarr = []
@@ -16,15 +14,36 @@ def rowCal(arr):
 		dd = defaultdict(lambda:0)
 		for num in row:
 			dd[num] += 1
-		newrow = sorted(dd.items, key=lambda x:(x[1]<<20)+x[0])
+		newrow = sorted(dd.items(), key=lambda x:(x[1]<<20)+x[0])
 		newarr.append([])
 		for i in newrow:
 			newarr[-1] += i
 		maxlen = max(maxlen, len(newarr[-1]))
+	maxlen = min(100, maxlen)
+	for i in range(len(newarr)):
+		if len(newarr[i]) > maxlen:
+			newarr[i] = newarr[i][0:maxlen]
+		else:
+			newarr[i] += [0 for _ in range(maxlen - len(newarr[i]))]
 	return newarr
 
 def colCal(arr):
-	newarr = [[arr[i][j] for i in range(len(arr[0]))] for j in range(len(arr))]
+	newarr = [[arr[i][j] for i in range(len(arr))] for j in range(len(arr[0]))]
 	newarr = rowCal(newarr)
+	return [[newarr[i][j] for i in range(len(newarr))] for j in range(len(newarr[0]))]
 
+def solve(A):
+	currentTime = 0
+	if A[r][c] == k:
+		return 0
+	while currentTime < 100:
+		currentTime += 1
+		if len(A) < len(A[0]):
+			A = colCal(A)
+		else:
+			A = rowCal(A)
+		if A[r][c] == k:
+			return currentTime
+	return -1
 
+print(solve(A))
